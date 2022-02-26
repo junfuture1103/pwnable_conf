ee0d19a386e81fe506d510582ae3ddf444894e24
JUNTHEWORLD <junhak1103@naver.com>
2022-02-27 07:45:40 +0900

    Update set_up.sh

--- set_up.sh
+++ set_up.sh
@@ -1,11 +1,17 @@
 #pwnable_conf.sh
 apt update -y
 apt-get update -y
+
+#vim
 apt install -y vim
+
+#pwntools
 apt install python -y
 apt-get install python2.7-dev python3-pip -y
-pip install pwntools -y
+pip install pwntools
 apt-get install libcapstone-dev -y
+
+#pwndbg
 sudo apt install -y git
 git clone https://github.com/pwndbg/pwndbg
 cd pwndbg
