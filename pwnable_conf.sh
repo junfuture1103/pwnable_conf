76d34358e18375761a59c4569e59b2d5fbc09b56
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2022-11-19 01:58:34 -0500

    Add code install

--- pwnable_conf.sh
+++ pwnable_conf.sh
@@ -5,6 +5,13 @@ sudo apt-get update -y
 #vim
 sudo apt install -y vim
 
+#visual studio code
+sudo apt-get install curl
+sudo sh -c 'curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /etc/apt/trusted.gpg.d/microsoft.gpg'
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt update
+sudo apt install code
+
 #pwntools
 sudo apt install python -y
 sudo apt-get install python2.7-dev python3-pip -y
