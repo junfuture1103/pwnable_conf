02cb9a2bca8301c018b97ed1dbbc0862b09f7d94
junfuture1103 <junhak1103@naver.com>
2023-02-04 07:53:17 +0000

    Modify setup.sh

--- setup.sh
+++ setup.sh
@@ -6,16 +6,16 @@ sudo apt-get update -y
 sudo apt install -y vim
 
 #visual studio code
-sudo apt-get install curl
+sudo apt-get install -y curl
 sudo sh -c 'curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /etc/apt/trusted.gpg.d/microsoft.gpg'
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt update
-sudo apt install code
+sudo apt update -y
+sudo apt install -y code
 
 #pwntools
 sudo apt install python -y
 sudo apt-get install python2.7-dev python3-pip -y
-sudo pip install pwntools
+sudo pip install -y pwntools
 sudo apt-get install libcapstone-dev -y
 
 #pwndbg
